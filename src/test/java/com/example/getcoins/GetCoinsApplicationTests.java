679fac9ad3d093a925e462ff2184ea11d1936334
Tuan Ngo <tuan.23a5@gmail.com>
2022-07-12 08:24:34 +0700

    change server port & update test

--- src/test/java/com/example/getcoins/GetCoinsApplicationTests.java
+++ src/test/java/com/example/getcoins/GetCoinsApplicationTests.java
@@ -1,13 +1,8 @@
 package com.example.getcoins;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GetCoinsApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
